5e7ca41fd68f90a7a0e8de21f691162c4d368839
jakethornton47 <nickenchuggets47@gmail.com>
2016-02-01 20:08:49 -0500

    Initial Commit

---
+++ Source/Project_Swaglord/Project_Swaglord.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+
+#include "Project_Swaglord.h"
+
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Project_Swaglord, "Project_Swaglord" );
+ 
---
+++ Source/Project_Swaglord/TileLayer.h
@@ -0,0 +1,69 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Tile.h"
+
+#include "GameFramework/Actor.h"
+#include "TileLayer.generated.h"
+
+UCLASS()
+class PROJECT_SWAGLORD_API ATileLayer : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ATileLayer();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawning")
+	FTransform LastSpawnTransform;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Tile")
+	TArray<ATile*> ActiveTiles;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawning")
+	float SpawnDelay = 2.5f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawning")
+	float SpawnAmount = 5;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Timer")
+	TArray<FTimerHandle> ActiveTimers;
+
+	UFUNCTION(BlueprintCallable, Category = "Spawning")
+	void SpawnTile(float numTiles);
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Spawning")
+	void SpawnTileEvent();
+
+
+private:
+
+
+	void NextSpawn(float delay);
+	void SpawnTile_Delegate();
+
+	__forceinline void UpdateGameOrigin(FTransform newTransform)
+	{
+		UWorld* world = GetWorld();
+		check(world);
+
+		FVector transform_vector = newTransform.GetLocation();
+
+		FIntVector vecNewOrigin = FIntVector(transform_vector.X, transform_vector.Y, transform_vector.Z) + world->OriginLocation;
+		world->RequestNewWorldOrigin(vecNewOrigin);
+
+
+	}
+
+	
+	
+};
